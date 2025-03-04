a199f670ce15044a969e1ef65c690abfa2ae8b9e
Mannat Sood <128911803+mannatsood08@users.noreply.github.com>
2025-03-04 18:18:00 +0530

    File updated at 3/4/2025, 6:18:00 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,10 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        res=[]
-        strset=set(nums)
+        r=[]
+        set_num=set(nums)
+        maxx=max(nums)
         for i in range(1,len(nums)+1):
-            if i not in strset:
-                res.append(i)
-        return res
+            if i not in set_num:
+                r.append(i)
+        return r
 
         
