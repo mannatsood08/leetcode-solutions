8c8654c3220dd32418994bd620c4b454231b380a
Mannat Sood <128911803+mannatsood08@users.noreply.github.com>
2025-03-16 19:24:49 +0530

    File updated at 3/16/2025, 7:24:49 PM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,16 +1,7 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) -1
-        
-        while i<j:
-            s = numbers[i] + numbers[j]
-            if s == target:
-                return [i + 1 , j + 1]
-            
-            if s > target:
-                j-=1
-            else:
-               i+=1 
-        
-        return []
+    def trailingZeroes(self, n: int) -> int:
+        res = 0
+        while n > 0:
+            n //= 5
+            res += n
+        return res
