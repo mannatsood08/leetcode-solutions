d8f7bd337ea4e1919328683f34501dd830012b9d
Mannat Sood <128911803+mannatsood08@users.noreply.github.com>
2025-07-04 16:56:53 +0530

    File updated at 7/4/2025, 4:56:53 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,18 +1,19 @@
-// Last updated: 7/1/2025, 12:40:34 PM
+// Last updated: 7/4/2025, 4:56:53 PM
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums == null || nums.length == 0 ){
-            return;
-        }
-        int cur =0;
-        for (int i = 0; i < nums.length; ++i) {
-            if(nums[i] != 0){
-                int temp = nums[cur];
-                nums[cur++] = nums[i];
-                nums[i] = temp;
+        int c = 0; // Pointer to place next non-zero element
+        
+        // Step 1: Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[c] = nums[i];
+                c++;
             }
         }
-
         
+        // Step 2: Fill remaining positions with 0
+        for (int i = c; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
