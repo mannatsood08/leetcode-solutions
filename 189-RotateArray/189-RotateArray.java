d8ab697d7faae59222b9f7e85a8e98324cfcc203
Mannat Sood <128911803+mannatsood08@users.noreply.github.com>
2025-07-05 23:06:23 +0530

    File updated at 7/5/2025, 11:06:23 PM

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,23 +1,26 @@
-// Last updated: 7/5/2025, 9:46:07 PM
+// Last updated: 7/5/2025, 11:06:23 PM
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; // handle k > n
-        int[] temp = new int[k];
-
-        // Step 1: Copy last k elements to temp
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
+        int n=nums.length;
+        k=k%n;
+        if(k<0){
+            k+=n;
         }
-
-        // Step 2: Shift the first (n - k) elements to the right
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i - k];
-        }
-
-        // Step 3: Copy temp to the beginning
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        
     }
+
+    public void reverse(int[] nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+
+         }
+    }
+
 }
